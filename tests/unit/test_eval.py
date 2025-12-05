736cada9aac43c4299f0d7588f414e8472b82837
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2025-12-05 17:52:43 -0500

    Test segmentation function.

--- tests/unit/test_eval.py
+++ tests/unit/test_eval.py
@@ -1,21 +1,23 @@
 """Test evaluation module."""
 
 import babyseg
+import katy
+import logging
 import pytest
 import torch
+import voxel as vx
 
 
-def test_select_dtype_values():
+@pytest.mark.parametrize('value, expected', [
+    (    0, torch.uint8),
+    (   -1, torch.int16),
+    (2**15, torch.uint16),
+    (2**16, torch.int32),
+])
+def test_select_dtype_values(value, expected):
     """Test selecting data types."""
-    expected = {
-            0: torch.uint8,
-           -1: torch.int16,
-        2**15: torch.uint16,
-        2**16: torch.int32
-    }
-    for x, y in expected.items():
-        x = torch.tensor(x)
-        assert babyseg.eval.select_dtype(x) == y
+    x = torch.tensor(value)
+    assert babyseg.eval.select_dtype(x) == expected
 
 
 def test_select_dtype_long():
@@ -25,9 +27,111 @@ def test_select_dtype_long():
         babyseg.eval.select_dtype(x)
 
 
-def test_select_dtype_illegal():
+@pytest.mark.parametrize('dtype', [torch.float32, torch.complex64])
+def test_select_dtype_illegal(dtype):
     """Test if selecting types for illegal input data raises errors."""
-    for dtype in (torch.float32, torch.complex64):
-        x = torch.tensor(1, dtype=dtype)
-        with pytest.raises(TypeError):
-            babyseg.eval.select_dtype(x)
+    x = torch.tensor(1, dtype=dtype)
+    with pytest.raises(TypeError):
+        babyseg.eval.select_dtype(x)
+
+
+@pytest.fixture
+def config(monkeypatch):
+    """Set up mock segmentation environment, return configuration."""
+    # Mock configuration.
+    labels = 3
+    config = {
+        'eval': {'orientation': 'LIA', 'spacing': 1, 'divisor': 2},
+        'synthesis': {'kwargs': {'optimize': 'some_path'}},
+    }
+
+    class Model:
+        def eval(self):
+            return self
+
+        def __call__(self, x):
+            """Return a tensor with `labels` channels, propagating NaN."""
+            return x.sum(dim=1, keepdim=True).expand(-1, labels, *x.shape[2:])
+
+    def load_model(*args, **kwargs):
+        return Model()
+
+    monkeypatch.setattr(katy.io, 'load', lambda _: range(labels))
+    monkeypatch.setattr(babyseg.config, 'load_model', load_model)
+    return config
+
+
+@pytest.fixture
+def inputs(monkeypatch):
+    """Map paths to segmentation inputs and feed them to `vx.load_volume`."""
+    inputs = {}
+    monkeypatch.setattr(vx, '_load_volume', vx.load_volume, raising=False)
+    monkeypatch.setattr(vx, 'load_volume', lambda f: inputs[f])
+    return inputs
+
+
+def test_segment_without_outputs(caplog):
+    """Test if segmenting without specifying outputs raises an error."""
+    caplog.set_level(logging.ERROR)
+    with pytest.raises(SystemExit) as e:
+        babyseg.eval.segment(config={}, images=[])
+
+    assert 'output' in caplog.text
+    assert e.value.code != 0
+
+
+def test_segment_eval_mode(monkeypatch):
+    """Test if segmentation sets evaluation mode."""
+    class Model:
+        pass
+
+    def load_model(*args, **kwargs):
+        return Model()
+
+    monkeypatch.setattr(babyseg.config, 'load_model', load_model)
+    with pytest.raises(AttributeError) as e:
+        babyseg.eval.segment(config={}, images=[], out_seg='out.nii')
+
+    assert e.value.name == 'eval'
+
+
+def test_segment_output_probabilities(config, inputs, tmp_path):
+    """Test output probability map properties."""
+    size = (2, 2, 2)
+    inputs['in.nii'] = vx.Volume(torch.ones(1, *size))
+    images = tuple(inputs)
+    f = tmp_path / 'out.nii.gz'
+
+    babyseg.eval.segment(config, images, out_prob=f)
+    out = vx._load_volume(f)
+    assert out.dtype == torch.float32
+    assert out.num_channels > 1
+    assert out.baseshape == size
+    assert out.min() >= 0
+    assert out.max() <= 1
+
+
+def test_segment_output_labels(config, inputs, tmp_path):
+    """Test output label map properties."""
+    size = (1, 2, 2, 2)
+    inputs['in.nii'] = vx.Volume(torch.ones(size))
+    images = tuple(inputs)
+    f = tmp_path / 'out.nii.gz'
+
+    babyseg.eval.segment(config, images, out_seg=f)
+    out = vx._load_volume(f)
+    assert out.dtype == torch.uint8
+    assert out.shape == size
+
+
+def test_segment_multiple_inputs(config, inputs, tmp_path):
+    """Test if segmenting multiple inputs yields one output label map."""
+    inputs['a.nii'] = vx.Volume(torch.ones(1, 2, 2, 2))
+    inputs['b.nii'] = vx.Volume(torch.ones(3, 1, 1, 1))
+    inputs['c.nii'] = vx.Volume(torch.ones(1, 3, 3, 3))
+    images = tuple(inputs)
+    f = tmp_path / 'out.nii.gz'
+
+    babyseg.eval.segment(config, images, out_seg=f)
+    out = vx._load_volume(f)
+    assert out.num_channels == 1

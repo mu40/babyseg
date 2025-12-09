48bc07c5d0dc42a2b03766e7ac490b32a00fb75d
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2025-12-08 19:01:08 -0500

    Test wrapper script in-process.

--- docker/wrapper.py
+++ docker/wrapper.py
@@ -22,104 +22,148 @@ import sys
 # environment variables `BABYSEG_TAG`, `BABYSEG_SIF`, and `BABYSEG_TOOL`.
 
 # Container version tag. Must exist.
-tag = '0.0'
+TAG = '0.0'
 
 # Local directory for storing Apptainer, Singularity images.
-sif = pathlib.Path(__file__).parent
+SIF = pathlib.Path(__file__).parent
 
 # Container tool preference. Checked left to right.
-tools = ('docker', 'apptainer', 'singularity', 'podman')
+TOOLS = ('docker', 'apptainer', 'singularity', 'podman')
 
 
-# Environment variables. Override settings above.
-def env(key, default):
-    """Return environment variable or default value."""
-    value = os.getenv(key)
+def error(message, code=1):
+    """Print a message to standard error, propagating an exit code.
+
+    Parameters
+    ----------
+    message : str
+        Error message.
+    code : int, optional
+        Exit code.
+
+    Returns
+    -------
+    int
+        Exit code.
+
+    """
+    print(f'ERROR: {message}', file=sys.stderr)
+    return code
+
+
+def read_env(name, default):
+    """Return an environment variable if set, otherwise a default value.
+
+    Parameters
+    ----------
+    name : str
+        Environment variable name.
+    default : object
+        Fallback value.
+
+    Returns
+    -------
+    object
+        Environment variable or fallback value.
+
+    """
+    value = os.getenv(name)
     if value:
-        print(f'Applying environment variable {key}="{value}"')
+        print(f'Applying environment variable {name}="{value}"')
         return value
+
     return default
 
-host = env('BABYSEG_MNT', os.getcwd())
-tag = env('BABYSEG_TAG', tag)
-sif = env('BABYSEG_SIF', sif)
-sif = pathlib.Path(sif) / f'babyseg_{tag}.sif'
 
-tools = env('BABYSEG_TOOL', tools)
-if isinstance(tools, str):
-    tools = (tools,)
+def main(argv=None):
+    """Entry point for command-line execution.
+
+    Parameters
+    ----------
+    argv : list of str, optional
+        Command-line arguments. If None, defaults to `sys.argv[1:]`.
+
+    """
+    # Environment variables. Override settings above.
+    host = read_env('BABYSEG_MNT', os.getcwd())
+    tag = read_env('BABYSEG_TAG', TAG)
+    sif = read_env('BABYSEG_SIF', SIF)
+    sif = pathlib.Path(sif) / f'babyseg_{tag}.sif'
+
+    tools = read_env('BABYSEG_TOOL', TOOLS)
+    if isinstance(tools, str):
+        tools = (tools,)
+
+    # Report version. Avoid errors when piping, for example, to `head`.
+    signal.signal(signal.SIGPIPE, handler=signal.SIG_DFL)
+    hub = 'https://hub.docker.com/u/freesurfer'
+    print(f'Running BabySeg version "{tag}" from {hub}')
+
+    # Find a container system.
+    for tool in tools:
+        tool = shutil.which(tool)
+        if tool:
+            tool = pathlib.Path(tool)
+            print(f'Selected "{tool}" to manage containers')
+            break
+
+    else:
+        return error(f'cannot locate container tool {tools}')
+
+    # Bind path and image URL. Mount BABYSEG_MNT as /mnt inside the container,
+    # which we made the working directory when building the image. Docker and
+    # Podman require absolute paths.
+    host = pathlib.Path(host).absolute()
+    print(f'Will bind /mnt in container to BABYSEG_MNT="{host}"')
+
+    image = f'freesurfer/babyseg:{tag}'
+    if tool.name != 'docker':
+        image = f'docker://{image}'
+
+    # Run Docker using the UID and GID of the host user. This user will own
+    # bind mounts in the container, preventing outputs owned by root. Root
+    # inside rootless Podman containers maps to the host user, which is what we
+    # want. If we set UID and GID inside the container to the non-root host
+    # user, as for Docker, these would get remapped according to /etc/subuid
+    # outside, causing permission problems. Pretty-print help text with `-t`.
+    if tool.name in ('docker', 'podman'):
+        arg = ('run', '--rm', '-v', f'{host}:/mnt', image)
+        if sys.stdout.isatty():
+            arg = (*arg[:-1], '-t', arg[-1])
+        if 'docker' in tool.name:
+            arg = (*arg[:-1], '-u', f'{os.getuid()}:{os.getgid()}', arg[-1])
+
+    # For Apptainer or Singularity, the users inside and outside the container
+    # are the same. The working directory is also the same, unless we set it.
+    elif tool.name in ('apptainer', 'singularity'):
+        if not sif.parent.is_dir():
+            return error('variable BABYSEG_SIF does not point to a directory')
+
+        if not sif.exists():
+            call = (tool, 'pull', sif, image)
+            print(f'Cannot find image "{sif}", pulling it')
+            print('Command:', *call)
+            p = subprocess.run(call)
+            if p.returncode != 0:
+                return p.returncode
+
+        arg = ('run', '--pwd', '/mnt', '-e', '-B', f'{host}:/mnt', sif)
+        if '-cu' in sif.name:
+            arg = (arg[0], '--nv', *arg[1:])
+
+    else:
+        return error(f'cannot set up unknown container tool "{tool}"')
+
+    # Default to command-line arguments if none provided.
+    if argv is None:
+        argv = sys.argv[1:]
+
+    # Summary, launch.
+    print('Command:', tool, *arg)
+    print('BabySeg arguments:', *argv)
+    p = subprocess.run((tool, *arg, *argv))
+    return p.returncode
 
 
-# Report version. Avoid errors when piping, for example, to `head`.
-signal.signal(signal.SIGPIPE, handler=signal.SIG_DFL)
-hub = 'https://hub.docker.com/u/freesurfer'
-print(f'Running BabySeg version "{tag}" from {hub}')
-
-
-# Find a container system.
-for tool in tools:
-    tool = shutil.which(tool)
-    if tool:
-        tool = pathlib.Path(tool)
-        print(f'Selected "{tool}" to manage containers')
-        break
-
-else:
-    print(f'Cannot locate container tool {tools}', file=sys.stderr)
-    exit(1)
-
-
-# Bind path and image URL. Mount BABYSEG_MNT as /mnt inside the container,
-# which we made the working directory when building the image. Docker and
-# Podman require absolute paths.
-host = pathlib.Path(host).absolute()
-print(f'Will bind /mnt in container to BABYSEG_MNT="{host}"')
-
-image = f'freesurfer/babyseg:{tag}'
-if tool.name != 'docker':
-    image = f'docker://{image}'
-
-
-# Run Docker containers with the UID and GID of the host user. This user will
-# own bind mounts inside the container, preventing output files owned by root.
-# Root inside rootless Podman containers maps to the host user, which is what
-# we want. If we set UID and GID inside the container to the non-root host user
-# as for Docker, then these would get remapped according to /etc/subuid
-# outside, causing permission problems. Pretty-print help text with `-t`.
-if tool.name in ('docker', 'podman'):
-    arg = ('run', '--rm', '-v', f'{host}:/mnt', image)
-    if sys.stdout.isatty():
-        arg = (*arg[:-1], '-t', arg[-1])
-    if 'docker' in tool.name:
-        arg = (*arg[:-1], '-u', f'{os.getuid()}:{os.getgid()}', arg[-1])
-
-
-# For Apptainer or Singularity, the users inside and outside the container are
-# the same. The working directory is also the same, unless we set it.
-elif tool.name in ('apptainer', 'singularity'):
-    if not sif.parent.is_dir():
-        print('BABYSEG_SIF does not point to a directory', file=sys.stderr)
-        exit(1)
-
-    if not sif.exists():
-        call = (tool, 'pull', sif, image)
-        print(f'Cannot find image "{sif}", pulling it')
-        print('Command:', *call)
-        p = subprocess.run(call)
-        if p.returncode:
-            exit(p.returncode)
-
-    arg = ('run', '--pwd', '/mnt', '-e', '-B', f'{host}:/mnt', sif)
-    if '-cu' in sif.name:
-        arg = (arg[0], '--nv', *arg[1:])
-
-else:
-    print(f'Cannot set up unknown container tool "{tool}"', file=sys.stderr)
-    exit(1)
-
-
-# Summary, launch.
-print('Command:', tool, *arg)
-print('BabySeg arguments:', *sys.argv[1:])
-p = subprocess.run((tool, *arg, *sys.argv[1:]))
-exit(p.returncode)
+if __name__ == '__main__':
+    raise SystemExit(main())
--- tests/unit/test_wrapper.py
+++ tests/unit/test_wrapper.py
@@ -1,241 +1,283 @@
 """Tests for the wrapper script."""
 
+import docker.wrapper
 import os
 import pathlib
 import pytest
 import subprocess
+import unittest.mock
 
 
 TOOLS_SIF = {'apptainer', 'singularity'}
 TOOLS_ALL = {'docker', 'podman', *TOOLS_SIF}
 
 
-@pytest.fixture
-def mock_tool(monkeypatch, tmp_path):
-    """Return factory to create a mock container tool that logs calls."""
-    def f(name, set_path=True, set_tool=True, code=0):
-        # Empty file.
-        log = tmp_path / f'{name}.log'
-        log.write_text('')
-
-        # Mock tool.
-        tool = tmp_path / name
-        tool.write_text(
-            '#!/bin/sh\n'
-            f'echo "$0" "$@" | tee -a "{log}"\n'
-            f'exit {code:d}\n'
-        )
-        tool.chmod(0o755)
-
-        # Environment.
-        if set_path:
-            path = os.getenv('PATH')
-            monkeypatch.setenv('PATH', f'{tmp_path}:{path}')
-        if set_tool:
-            monkeypatch.setenv('BABYSEG_TOOL', tool.name)
-
-        return tool, log
-
-    return f
-
-
-def run_wrapper():
-    """Run the container script without relying on `PATH`."""
-    return subprocess.run('docker/wrapper.py')
-
-
-def test_environment(monkeypatch):
-    """Test the test environment."""
+@pytest.fixture(scope='session')
+def docker_name():
+    """Provide a validated Docker image name for tests."""
     name = os.getenv('BABYSEG_DOCKER_NAME')
-    assert name is not None
-    assert not name.startswith('/')
-    assert not name.endswith('/')
-    assert '/' in name
+    if not name or '/' not in name:
+        pytest.fail('invalid or missing BABYSEG_DOCKER_NAME')
+
+    return name
 
 
-def construct_sif_file(folder, tag, touch=False):
-    """Construct SIF file path from folder, tag, and `BABYSEG_DOCKER_NAME`."""
-    f = os.getenv('BABYSEG_DOCKER_NAME')
-    f = pathlib.Path(f).name
-    f = pathlib.Path(folder) / f'{f}_{tag}.sif'
-    if touch:
-        f.touch()
+@pytest.fixture
+def sif_factory(docker_name):
+    """Return factory to construct SIF file paths."""
+    def sif_file(folder, tag, touch=False):
+        f = pathlib.Path(docker_name).name
+        f = pathlib.Path(folder) / f'{f}_{tag}.sif'
+        if touch:
+            f.touch()
+
+        return f
+
+    return sif_file
+
+
+@pytest.fixture
+def mock_factory(monkeypatch, tmp_path):
+    """Return factory to create a mock container tool that logs calls."""
+    mock = unittest.mock.create_autospec(subprocess.run)
+    monkeypatch.setattr(subprocess, 'run', mock)
+
+    def f(name, set_path=True, set_tool=True, code=0):
+        tool = tmp_path / name
+        tool.touch(mode=0o755)
+        monkeypatch.setenv('PATH', str(tmp_path))
+        monkeypatch.setenv('BABYSEG_TOOL', tool.name)
+
+        mock.return_value = subprocess.CompletedProcess(
+            args='',
+            returncode=code,
+        )
+        return tool, mock
 
     return f
 
 
-@pytest.mark.parametrize('name', TOOLS_ALL)
-def test_tool_auto(mock_tool, name):
-    """Test auto-selecting tools from `PATH`."""
-    tool, log = mock_tool(name)
+@pytest.mark.parametrize('tool', TOOLS_ALL)
+def test_shell(tool, tmp_path, sif_factory, monkeypatch):
+    """Verify entry point shebang, executable bit, CLI argument handling."""
+    code = 123
+    arg = ('-A', '-B', 'file.json')
 
-    # Expect no log initially.
-    assert not log.read_text()
-    assert run_wrapper().returncode == 0
-    assert log.read_text().startswith(str(tool))
+    # Mock tool.
+    tool = tmp_path / tool
+    tool.write_text(f'#!/bin/sh\necho "$@" >/dev/stderr\nexit {code:d}\n')
+    tool.chmod(0o755)
 
-
-@pytest.mark.parametrize('name', TOOLS_ALL)
-def test_tool_absolute(mock_tool, monkeypatch, name):
-    """Test running a tool from absolute path when not in `PATH`."""
-    tool, log = mock_tool(name, set_path=False, set_tool=False)
+    # Create SIF file, so SIF tools skip the `pull` call.
+    sif = sif_factory(tmp_path, docker.wrapper.TAG, touch=True)
+    monkeypatch.setenv('BABYSEG_SIF', str(sif.parent))
     monkeypatch.setenv('BABYSEG_TOOL', str(tool))
 
-    assert run_wrapper().returncode == 0
-    assert log.read_text().startswith(str(tool))
+    call = (docker.wrapper.__file__, *arg)
+    p = subprocess.run(call, capture_output=True, text=True)
+    assert p.returncode == code
+    assert p.stderr.strip().endswith(' '.join(arg))
 
 
-def test_tool_missing(monkeypatch):
+@pytest.mark.parametrize('tool', TOOLS_ALL)
+def test_tool_auto(mock_factory, tool):
+    """Test auto-selecting tools from `PATH`."""
+    tool, mock = mock_factory(tool)
+    assert docker.wrapper.main(argv=[]) == 0
+    mock.assert_called()
+    (call,) = mock.call_args.args
+    assert call[0] == tool
+
+
+@pytest.mark.parametrize('tool', TOOLS_ALL)
+def test_tool_absolute(mock_factory, tool, monkeypatch):
+    """Test running a tool from absolute path when not in `PATH`."""
+    tool, mock = mock_factory(tool)
+    monkeypatch.delenv('PATH')
+    monkeypatch.setenv('BABYSEG_TOOL', str(tool))
+
+    assert docker.wrapper.main(argv=[]) == 0
+    mock.assert_called()
+    (call,) = mock.call_args.args
+    assert call[0] == tool
+
+
+def test_tool_missing(monkeypatch, capteesys):
     """Test if setting a tool that does not exist raises an error."""
-    monkeypatch.setenv('BABYSEG_TOOL', '/an/unknown/tool')
-    assert run_wrapper().returncode
+    monkeypatch.setenv('BABYSEG_TOOL', '/some/missing/tool')
+    assert docker.wrapper.main(argv=[]) > 0
+    f = capteesys.readouterr()
+    assert 'cannot locate' in f.err
 
 
-def test_tool_unknown(mock_tool):
-    """Test if setting an unknown but existing tool raises an error."""
-    mock_tool('fancy-container-tool')
-    assert run_wrapper().returncode
+def test_tool_unknown(mock_factory, capteesys):
+    """Test if setting an unknown existing tool raises an error."""
+    mock_factory('some-fancy-tool')
+    assert docker.wrapper.main(argv=[]) > 0
+    f = capteesys.readouterr()
+    assert 'unknown container tool' in f.err
 
 
-@pytest.mark.parametrize('name', TOOLS_ALL)
-def test_user(mock_tool, monkeypatch, name):
+@pytest.mark.parametrize('tool', TOOLS_ALL)
+def test_user(mock_factory, tool):
     """Test which tools specify user and group."""
-    tag = '0.0'
-    tool, log = mock_tool(name)
+    tool, mock = mock_factory(tool)
+    assert docker.wrapper.main(argv=[]) == 0
 
-    # Point to existing SIF file.
-    sif = construct_sif_file(tool.parent, tag, touch=True)
-    monkeypatch.setenv('BABYSEG_TAG', tag)
-    monkeypatch.setenv('BABYSEG_SIF', str(sif.parent))
-    assert run_wrapper().returncode == 0
-
-    # Expect UID, GID setting only for Docker.
-    is_docker = name == 'docker'
-    is_user = f'{os.getuid()}:{os.getgid()}' in log.read_text().split()
-    assert is_user == is_docker
+    # Expect UID, GID in the final `run` call only for Docker.
+    mock.assert_called()
+    (call,) = mock.call_args.args
+    assert (f'{os.getuid()}:{os.getgid()}' in call) == (tool.name == 'docker')
 
 
-def test_docker_run(mock_tool, monkeypatch):
+def test_docker_run(mock_factory, monkeypatch, docker_name):
     """Test the presence of flags in a Docker call."""
-    tag = '0.0'
-    tool, log = mock_tool('docker')
+    tool, mock = mock_factory('docker')
+    tag = 'latest'
     monkeypatch.setenv('BABYSEG_TAG', tag)
-    assert run_wrapper().returncode == 0
 
-    out = log.read_text().split()
-    assert out[0] == str(tool)
-    assert out[1] == 'run'
-    assert '--rm' in out
-    assert f'{os.getcwd()}:/mnt' in out
-    assert out[-1] == os.getenv('BABYSEG_DOCKER_NAME') + f':{tag}'
+    # Expect a single `run` call.
+    assert docker.wrapper.main(argv=[]) == 0
+    mock.assert_called_once()
+
+    (call,) = mock.call_args.args
+    assert call[:2] == (tool, 'run')
+    assert '--rm' in call
+    assert f'{os.getcwd()}:/mnt' in call
+    assert call[-1] == f'{docker_name}:{tag}'
 
 
-@pytest.mark.parametrize('name', TOOLS_SIF)
-def test_sif_directory_error(mock_tool, monkeypatch, name):
+@pytest.mark.parametrize('tool', TOOLS_SIF)
+def test_sif_directory_error(mock_factory, tool, monkeypatch, capteesys):
     """Test if not pointing `BABYSEG_SIF` to a directory raises an error."""
-    tool, log = mock_tool(name)
+    # Mock tool to avoid pulls.
+    tool, _ = mock_factory(tool)
 
-    # Expect failure on an existing file
-    d = log
-    monkeypatch.setenv('BABYSEG_SIF', str(d))
-    assert run_wrapper().returncode != 0
+    # Expect failure on an existing file.
+    monkeypatch.setenv('BABYSEG_SIF', str(tool))
+    assert docker.wrapper.main(argv=[]) > 0
+    assert 'directory' in capteesys.readouterr().err
 
-    # Expect failure on a directory that does not exist.
-    d = tool.parent / 'a' / 'b'
-    monkeypatch.setenv('BABYSEG_SIF', str(d))
-    assert run_wrapper().returncode != 0
+    # Expect failure on a path that does not exist.
+    monkeypatch.setenv('BABYSEG_SIF', str(tool.parent / 'missing'))
+    assert docker.wrapper.main(argv=[]) > 0
+    assert 'directory' in capteesys.readouterr().err
 
-    # Expect success on an existing directory.
-    d = tool.parent / 'existing'
-    d.mkdir()
-    monkeypatch.setenv('BABYSEG_SIF', str(d))
-    assert run_wrapper().returncode == 0
+    # Expect success on a directory.
+    monkeypatch.setenv('BABYSEG_SIF', str(tool.parent))
+    assert docker.wrapper.main(argv=[]) == 0
 
 
-@pytest.mark.parametrize('name', TOOLS_SIF)
-def test_sif_file_absent(mock_tool, monkeypatch, name):
+@pytest.mark.parametrize('tool', TOOLS_SIF)
+def test_sif_pull(mock_factory, tool, monkeypatch, docker_name, sif_factory):
     """Test behavior when the SIF file is missing and `BABYSEG_SIF` unset."""
+    tool, mock = mock_factory(tool)
     tag = 'absent'
-    tool, log = mock_tool(name)
     monkeypatch.setenv('BABYSEG_TAG', tag)
-    p = run_wrapper()
-    assert p.returncode == 0
 
-    # Expect default SIF path alongside script.
-    image = os.getenv('BABYSEG_DOCKER_NAME')
-    d = pathlib.Path(p.args).absolute().parent
-    sif = construct_sif_file(d, tag, touch=False)
-    hub = f'docker://{image}:{tag}'
+    # Expect `pull` and `run` calls for SIF tools.
+    assert docker.wrapper.main(argv=[]) == 0
+    assert mock.call_count == 2
 
-    # Expect two calls; `pull` first.
-    out = log.read_text().splitlines()
-    assert len(out) == 2
-    assert out[0].split() == [str(tool), 'pull', str(sif), hub]
+    # Default image path and URL.
+    d = pathlib.Path(docker.wrapper.__file__).parent
+    sif = sif_factory(d, tag, touch=False)
+    hub = f'docker://{docker_name}:{tag}'
+
+    # Expect valid `pull` arguments.
+    (first,) = mock.call_args_list[0].args
+    assert first == (tool, 'pull', sif, hub)
 
 
-@pytest.mark.parametrize('name', TOOLS_SIF)
-def test_sif_file_present(mock_tool, monkeypatch, name):
+@pytest.mark.parametrize('tool', TOOLS_SIF)
+def test_sif_run(mock_factory, tool, monkeypatch, sif_factory):
     """Test behavior when the SIF file exists."""
-    tag = '9.0'
-    tool, log = mock_tool(name)
-    sif = construct_sif_file(tool.parent, tag, touch=True)
-    monkeypatch.setenv('BABYSEG_TAG', tag)
+    tool, mock = mock_factory(tool)
+    sif = sif_factory(tool.parent, docker.wrapper.TAG, touch=True)
     monkeypatch.setenv('BABYSEG_SIF', str(sif.parent))
-    p = run_wrapper()
-    assert p.returncode == 0
 
     # Expect `run` call  only.
-    assert len(log.read_text().splitlines()) == 1
+    assert docker.wrapper.main(argv=[]) == 0
+    mock.assert_called_once()
 
-    run = log.read_text().split()
-    assert run[:2] == [str(tool), 'run']
-    assert '--pwd' in run
-    assert '/mnt' in run
-    assert f'{os.getcwd()}:/mnt' in run
-    assert run[-1] == str(sif)
+    # Expect valid `run` arguments.
+    (call,) = mock.call_args.args
+    assert call[:2] == (tool, 'run')
+    assert '--pwd' in call
+    assert '/mnt' in call
+    assert f'{os.getcwd()}:/mnt' in call
+    assert call[-1] == sif
 
 
-@pytest.mark.parametrize('name', TOOLS_ALL)
-def test_bind_mount(mock_tool, monkeypatch, name):
+@pytest.mark.parametrize('tool', TOOLS_ALL)
+def test_bind_mount(mock_factory, tool, monkeypatch):
     """Test explicit bind mount of `/mnt` inside container."""
+    tool, mock = mock_factory(tool)
     d = '/a/b/c'
-    _, log = mock_tool(name)
     monkeypatch.setenv('BABYSEG_MNT', d)
 
-    assert run_wrapper().returncode == 0
-    assert f'{d}:/mnt' in log.read_text().split()
+    # Expect one or two calls.
+    assert docker.wrapper.main(argv=[]) == 0
+    mock.assert_called()
+
+    # Expect bind mount in final `run` call.
+    (call,) = mock.call_args.args
+    assert f'{d}:/mnt' in call
 
 
-@pytest.mark.parametrize('name', TOOLS_SIF)
-def test_gpu(mock_tool, monkeypatch, name):
+@pytest.mark.parametrize('tool', TOOLS_SIF)
+@pytest.mark.parametrize('tag', ['1.2.3-cu130', '9.9'])
+def test_sif_gpu(mock_factory, tool, monkeypatch, tag):
     """Test enabling GPU support via image tag."""
-    tool, log = mock_tool(name)
-    monkeypatch.setenv('BABYSEG_SIF', str(tool.parent))
+    tool, mock = mock_factory(tool)
+    monkeypatch.setenv('BABYSEG_TAG', tag)
+    assert docker.wrapper.main(argv=[]) == 0
 
-    # Expect GPU enabled when `-cu` in tag.
-    for tag in ('1.2.3-cu130', '9.9'):
-        log.write_text('')
-        construct_sif_file(os.getenv('BABYSEG_SIF'), tag, touch=True)
-        monkeypatch.setenv('BABYSEG_TAG', tag)
-        assert run_wrapper().returncode == 0
-
-        is_gpu_image = '-cu' in tag
-        is_gpu_enabled = '--nv' in log.read_text().split()
-        assert is_gpu_image == is_gpu_enabled
+    # Expect GPU flag when `-cu` in tag.
+    mock.assert_called()
+    (call,) = mock.call_args.args
+    assert ('-cu' in tag) == ('--nv' in call)
 
 
-@pytest.mark.parametrize('name', TOOLS_SIF)
-def test_error_code_on_pull(mock_tool, monkeypatch, name):
+@pytest.mark.parametrize('tool', TOOLS_SIF)
+def test_error_code_on_pull(mock_factory, tool, monkeypatch):
     """Test if failure on SIF image `pull` returns the correct code."""
+    # Set BABYSEG_SIF to a directory without SIF images.
+    tool, mock = mock_factory(tool)
+    monkeypatch.setenv('BABYSEG_SIF', str(tool.parent))
     code = 7
-    mock_tool(name, code=code)
-    monkeypatch.setenv('BABYSEG_TAG', '2.0-missing')
-    assert run_wrapper().returncode == code
+    mock.return_value = subprocess.CompletedProcess(args='', returncode=code)
+
+    # Expect exit after failed pull.
+    assert docker.wrapper.main(argv=[]) == code
+    mock.assert_called_once()
 
 
-@pytest.mark.parametrize('name', TOOLS_ALL - TOOLS_SIF)
-def test_error_code_on_run(mock_tool, monkeypatch, name):
+@pytest.mark.parametrize('tool', TOOLS_ALL)
+def test_error_code_on_run(mock_factory, tool, monkeypatch, sif_factory):
     """Test if failure on `run` returns the correct code."""
+    tool, mock = mock_factory(tool)
     code = 13
-    mock_tool(name, code=code)
-    assert run_wrapper().returncode == code
+    mock.return_value = subprocess.CompletedProcess(args='', returncode=code)
+    sif = sif_factory(tool.parent, docker.wrapper.TAG, touch=True)
+    monkeypatch.setenv('BABYSEG_SIF', str(sif.parent))
+
+    # Expect single call as SIF file exists.
+    assert docker.wrapper.main(argv=[]) == code
+    mock.assert_called_once()
+
+
+@pytest.mark.parametrize('tool', TOOLS_ALL)
+def test_arguments(mock_factory, tool, sif_factory, monkeypatch):
+    """Test if the wrapper forwards input arguments."""
+    # Create SIF file, so SIF tools skip the `pull` call.
+    argv = ('-a', '-B','file.json')
+    tool, mock = mock_factory(tool)
+    sif = sif_factory(tool.parent, docker.wrapper.TAG, touch=True)
+    monkeypatch.setenv('BABYSEG_SIF', str(sif.parent))
+
+    assert docker.wrapper.main(argv) == 0
+    mock.assert_called_once()
+    (call,) = mock.call_args.args
+    print(call)
+    assert call[-len(argv):] == argv

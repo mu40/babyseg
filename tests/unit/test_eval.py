e0585e0cf3bf04c96509b87853b25f8a66c78f2f
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2025-12-08 19:02:15 -0500

    Test evaluation model setup and output geometry.

--- tests/unit/test_eval.py
+++ tests/unit/test_eval.py
@@ -4,7 +4,7 @@ import babyseg
 import katy
 import pytest
 import torch
-import unittest
+import unittest.mock
 import voxel as vx
 
 
@@ -41,20 +41,18 @@ def config(monkeypatch):
     # Mock configuration.
     labels = 3
     config = {
-        'eval': {'orientation': 'LIA', 'spacing': 1, 'divisor': 2},
-        'synthesis': {'kwargs': {'optimize': 'some_path'}},
+        'eval': {'orientation': 'LIA', 'spacing': 1.5, 'divisor': 2},
+        'synthesis': {'kwargs': {'optimize': 'labels.json'}},
     }
 
     # Return tensors with `labels` channels, propagating NaN.
-    class Model:
-        def eval(self):
-            return self
+    def forward(x):
+        return x.sum(dim=1, keepdim=True).expand(-1, labels, *x.shape[2:])
 
-        def __call__(self, x):
-            return x.sum(dim=1, keepdim=True).expand(-1, labels, *x.shape[2:])
-
+    model = unittest.mock.Mock(side_effect=forward)
+    load_model = unittest.mock.Mock(return_value=model)
+    monkeypatch.setattr(babyseg.config, 'load_model', load_model)
     monkeypatch.setattr(katy.io, 'load', lambda _: range(labels))
-    monkeypatch.setattr(babyseg.config, 'load_model', lambda *_, **__: Model())
     return config
 
 
@@ -73,41 +71,116 @@ def test_segment_without_outputs():
         babyseg.eval.segment(config={}, images=[])
 
 
+def test_segment_load_config(monkeypatch):
+    """Test if segmentation loads a specified configuration."""
+    mock = unittest.mock.Mock(side_effect=RuntimeError('LOAD'))
+    monkeypatch.setattr(babyseg.config, 'load', mock)
+
+    image = 'in.nii'
+    config = 'config.json'
+    with pytest.raises(RuntimeError, match='LOAD'):
+        babyseg.eval.segment(config, image)
+
+    mock.assert_called_once_with(config)
+
+
+def test_segment_load_model(monkeypatch):
+    """Test if segmentation passes settings to model setup."""
+    mock = unittest.mock.create_autospec(babyseg.config.load_model)
+    mock.side_effect = RuntimeError('LOAD_MODEL')
+    monkeypatch.setattr(babyseg.config, 'load_model', mock)
+
+    checkpoint = 'checkpoint.pt'
+    device = 'GPU'
+    with pytest.raises(RuntimeError, match='LOAD_MODEL'):
+        babyseg.eval.segment(
+            config={},
+            images='in.nii',
+            out_seg='out.nii',
+            checkpoint=checkpoint,
+            device=device,
+        )
+
+    mock.assert_called_once()
+    args, kwargs = mock.call_args
+    received = (*args, *kwargs.values())
+    assert checkpoint in received
+    assert device in received
+
+
 def test_segment_eval_mode(monkeypatch):
     """Test if segmentation sets evaluation mode."""
-    mock = unittest.mock.Mock(side_effect=RuntimeError('EVAL'))
+    model = unittest.mock.Mock()
+    model.eval.side_effect = RuntimeError('EVAL')
+
+    mock = unittest.mock.Mock(return_value=model)
     monkeypatch.setattr(babyseg.config, 'load_model', mock)
+
     with pytest.raises(RuntimeError, match='EVAL'):
         babyseg.eval.segment(config={}, images=[], out_seg='out.nii')
+
+    model.eval.assert_called_with()
 
 
 def test_segment_output_probabilities(config, inputs, tmp_path):
     """Test output probability map properties."""
-    size = (2, 2, 2)
-    inputs['in.nii'] = vx.Volume(torch.ones(1, *size))
+    x = vx.Volume(torch.ones(1, 2, 2, 2))
+    inputs['in.nii'] = x
     images = tuple(inputs)
     f = tmp_path / 'out.nii.gz'
 
     babyseg.eval.segment(config, images, out_prob=f)
     out = vx._load_volume(f)
+
+    # Expect probabilities.
     assert out.dtype == torch.float32
-    assert out.num_channels > 1
-    assert out.baseshape == size
     assert out.min() >= 0
     assert out.max() <= 1
+
+    # Expect same geometry as input, more than one channel.
+    assert out.num_channels > 1
+    assert out.baseshape == x.shape[1:]
+    assert out.geometry.orientation == x.geometry.orientation
+    assert out.geometry.spacing.equal(x.geometry.spacing)
 
 
 def test_segment_output_labels(config, inputs, tmp_path):
     """Test output label map properties."""
-    size = (1, 2, 2, 2)
-    inputs['in.nii'] = vx.Volume(torch.ones(size))
+    x = vx.Volume(torch.ones(1, 2, 2, 2))
+    inputs['in.nii'] = x
     images = tuple(inputs)
     f = tmp_path / 'out.nii.gz'
 
     babyseg.eval.segment(config, images, out_seg=f)
     out = vx._load_volume(f)
+
+    # Expect smallest integer type for small constant-valued input.
     assert out.dtype == torch.uint8
-    assert out.shape == size
+
+    # Expect same geometry as input, one channel.
+    assert out.shape == x.shape
+    assert out.geometry.orientation == x.geometry.orientation
+    assert out.geometry.spacing.equal(x.geometry.spacing)
+
+
+def test_segment_output_lead(config, inputs, tmp_path):
+    """Test conformed lead image properties."""
+    size = (1, 2, 2, 2)
+    images = 'in.nii'
+    inputs[images] = vx.Volume(torch.ones(size))
+    f = tmp_path / 'out.nii.gz'
+
+    babyseg.eval.segment(config, images, out_lead=f)
+    out = vx._load_volume(f)
+
+    # Expect hard coded type and shape, number of input channels.
+    assert out.dtype == torch.float32
+    assert out.baseshape == (128, 128, 128)
+    assert out.num_channels == size[0]
+
+    # Expect configuration geometry.
+    assert out.geometry.orientation == config['eval']['orientation']
+    assert out.geometry.spacing.eq(config['eval']['spacing']).all()
 
 
 def test_segment_nan(config, inputs, tmp_path):

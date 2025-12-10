64ad115c5507f5c9177259c1ae4b5fc9de5f58f3
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2025-12-10 13:31:04 -0500

    Exclude slow test.

--- tests/system/test_install.py
+++ tests/system/test_install.py
@@ -1,9 +1,11 @@
 """Tests for environment install."""
 
+import pytest
 import subprocess
 import venv
 
 
+@pytest.mark.slow
 def test_clone_environment(tmp_path):
     """Test environment install from requirements file."""
     venv.EnvBuilder(with_pip=True).create(tmp_path)

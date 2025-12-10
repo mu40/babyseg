1e172757b78545888ab6743f6caa016dfe66e853
Malte Hoffmann <mhoffmann@mgh.harvard.edu>
2025-12-10 13:27:38 -0500

    Parameterize indirectly instead of returning a factory.

--- tests/unit/test_wrapper.py
+++ tests/unit/test_wrapper.py
@@ -15,11 +15,13 @@ TOOLS_ALL = {'docker', 'podman', *TOOLS_SIF}
 @pytest.fixture(scope='session')
 def docker_name():
     """Provide a validated Docker image name for tests."""
-    name = os.getenv('BABYSEG_DOCKER_NAME')
-    if not name or '/' not in name:
-        pytest.fail('invalid or missing BABYSEG_DOCKER_NAME')
+    name = 'BABYSEG_DOCKER_NAME'
+    value = os.getenv(name)
+    if not value:
+        pytest.skip(f'missing {name}')
 
-    return name
+    assert '/' in value, f'invalid {name} value "{value}"'
+    return value
 
 
 @pytest.fixture
@@ -37,29 +39,30 @@ def sif_factory(docker_name):
 
 
 @pytest.fixture
-def mock_factory(monkeypatch, tmp_path):
-    """Return factory to create a mock container tool that logs calls."""
-    mock = unittest.mock.create_autospec(subprocess.run)
-    monkeypatch.setattr(subprocess, 'run', mock)
+def tool(monkeypatch, tmp_path, request):
+    """Create a mock container tool that logs calls."""
+    name = request.param
+    path = tmp_path / name
+    path.touch(mode=0o755)
 
-    def f(name, set_path=True, set_tool=True, code=0):
-        tool = tmp_path / name
-        tool.touch(mode=0o755)
-        monkeypatch.setenv('PATH', str(tmp_path))
-        monkeypatch.setenv('BABYSEG_TOOL', tool.name)
+    tool = unittest.mock.create_autospec(subprocess.run)
+    tool.name = name
+    tool.path = path
+    tool.return_value = subprocess.CompletedProcess(args='', returncode=0)
 
-        mock.return_value = subprocess.CompletedProcess(
-            args='',
-            returncode=code,
-        )
-        return tool, mock
-
-    return f
+    # Clear environment variables to ensure the wrapper uses defaults.
+    monkeypatch.delenv('BABYSEG_MNT', raising=False)
+    monkeypatch.delenv('BABYSEG_SIF', raising=False)
+    monkeypatch.delenv('BABYSEG_TAG', raising=False)
+    monkeypatch.setenv('BABYSEG_TOOL', name)
+    monkeypatch.setenv('PATH', str(tmp_path))
+    monkeypatch.setattr(subprocess, 'run', tool)
+    return tool
 
 
 @pytest.mark.parametrize('tool', TOOLS_ALL)
 def test_shell(tool, tmp_path, sif_factory, monkeypatch):
-    """Verify entry point shebang, executable bit, CLI argument handling."""
+    """Verify entry-point shebang, executable bit, CLI argument handling."""
     code = 123
     arg = ('-A', '-B', 'file.json')
 
@@ -68,7 +71,7 @@ def test_shell(tool, tmp_path, sif_factory, monkeypatch):
     tool.write_text(f'#!/bin/sh\necho "$@" >/dev/stderr\nexit {code:d}\n')
     tool.chmod(0o755)
 
-    # Create SIF file, so SIF tools skip the `pull` call.
+    # Create dummy SIF file, so SIF tools skip the `pull` call.
     sif = sif_factory(tmp_path, docker.wrapper.TAG, touch=True)
     monkeypatch.setenv('BABYSEG_SIF', str(sif.parent))
     monkeypatch.setenv('BABYSEG_TOOL', str(tool))
@@ -79,105 +82,98 @@ def test_shell(tool, tmp_path, sif_factory, monkeypatch):
     assert p.stderr.strip().endswith(' '.join(arg))
 
 
-@pytest.mark.parametrize('tool', TOOLS_ALL)
-def test_tool_auto(mock_factory, tool):
+@pytest.mark.parametrize('tool', TOOLS_ALL, indirect=True)
+def test_tool_auto(tool):
     """Test auto-selecting tools from `PATH`."""
-    tool, mock = mock_factory(tool)
     assert docker.wrapper.main(argv=[]) == 0
-    mock.assert_called()
-    (call,) = mock.call_args.args
-    assert call[0] == tool
+    tool.assert_called()
+    (call,) = tool.call_args.args
+    assert call[0] == tool.path
 
 
-@pytest.mark.parametrize('tool', TOOLS_ALL)
-def test_tool_absolute(mock_factory, tool, monkeypatch):
+@pytest.mark.parametrize('tool', TOOLS_ALL, indirect=True)
+def test_tool_absolute(tool, monkeypatch):
     """Test running a tool from absolute path when not in `PATH`."""
-    tool, mock = mock_factory(tool)
     monkeypatch.delenv('PATH')
-    monkeypatch.setenv('BABYSEG_TOOL', str(tool))
+    monkeypatch.setenv('BABYSEG_TOOL', str(tool.path))
 
     assert docker.wrapper.main(argv=[]) == 0
-    mock.assert_called()
-    (call,) = mock.call_args.args
-    assert call[0] == tool
+    tool.assert_called()
+    (call,) = tool.call_args.args
+    assert call[0] == tool.path
 
 
 def test_tool_missing(monkeypatch, capteesys):
-    """Test if setting a tool that does not exist raises an error."""
+    """Verify that a missing tool raises an error."""
     monkeypatch.setenv('BABYSEG_TOOL', '/some/missing/tool')
     assert docker.wrapper.main(argv=[]) > 0
     f = capteesys.readouterr()
     assert 'cannot locate' in f.err
 
 
-def test_tool_unknown(mock_factory, capteesys):
-    """Test if setting an unknown existing tool raises an error."""
-    mock_factory('some-fancy-tool')
+@pytest.mark.parametrize('tool', ['some-fancy-tool'], indirect=True)
+def test_tool_unknown(tool, capteesys):
+    """Verify that an unknown existing tool raises an error."""
     assert docker.wrapper.main(argv=[]) > 0
     f = capteesys.readouterr()
     assert 'unknown container tool' in f.err
 
 
-@pytest.mark.parametrize('tool', TOOLS_ALL)
-def test_user(mock_factory, tool):
+@pytest.mark.parametrize('tool', TOOLS_ALL, indirect=True)
+def test_user(tool):
     """Test which tools specify user and group."""
-    tool, mock = mock_factory(tool)
     assert docker.wrapper.main(argv=[]) == 0
 
     # Expect UID, GID in the final `run` call only for Docker.
-    mock.assert_called()
-    (call,) = mock.call_args.args
+    tool.assert_called()
+    (call,) = tool.call_args.args
     assert (f'{os.getuid()}:{os.getgid()}' in call) == (tool.name == 'docker')
 
 
-def test_docker_run(mock_factory, monkeypatch, docker_name):
+@pytest.mark.parametrize('tool', ['docker'], indirect=True)
+def test_docker_run(tool, monkeypatch, docker_name):
     """Test the presence of flags in a Docker call."""
-    tool, mock = mock_factory('docker')
     tag = 'latest'
     monkeypatch.setenv('BABYSEG_TAG', tag)
 
     # Expect a single `run` call.
     assert docker.wrapper.main(argv=[]) == 0
-    mock.assert_called_once()
+    tool.assert_called_once()
 
-    (call,) = mock.call_args.args
-    assert call[:2] == (tool, 'run')
+    (call,) = tool.call_args.args
+    assert call[:2] == (tool.path, 'run')
     assert '--rm' in call
     assert f'{os.getcwd()}:/mnt' in call
     assert call[-1] == f'{docker_name}:{tag}'
 
 
-@pytest.mark.parametrize('tool', TOOLS_SIF)
-def test_sif_directory_error(mock_factory, tool, monkeypatch, capteesys):
+@pytest.mark.parametrize('tool', TOOLS_SIF, indirect=True)
+def test_sif_directory_error(tool, monkeypatch, capteesys):
     """Test if not pointing `BABYSEG_SIF` to a directory raises an error."""
-    # Mock tool to avoid pulls.
-    tool, _ = mock_factory(tool)
-
     # Expect failure on an existing file.
-    monkeypatch.setenv('BABYSEG_SIF', str(tool))
+    monkeypatch.setenv('BABYSEG_SIF', str(tool.path))
     assert docker.wrapper.main(argv=[]) > 0
     assert 'directory' in capteesys.readouterr().err
 
     # Expect failure on a path that does not exist.
-    monkeypatch.setenv('BABYSEG_SIF', str(tool.parent / 'missing'))
+    monkeypatch.setenv('BABYSEG_SIF', str(tool.path.parent / 'missing'))
     assert docker.wrapper.main(argv=[]) > 0
     assert 'directory' in capteesys.readouterr().err
 
     # Expect success on a directory.
-    monkeypatch.setenv('BABYSEG_SIF', str(tool.parent))
+    monkeypatch.setenv('BABYSEG_SIF', str(tool.path.parent))
     assert docker.wrapper.main(argv=[]) == 0
 
 
-@pytest.mark.parametrize('tool', TOOLS_SIF)
-def test_sif_pull(mock_factory, tool, monkeypatch, docker_name, sif_factory):
+@pytest.mark.parametrize('tool', TOOLS_SIF, indirect=True)
+def test_sif_pull(tool, monkeypatch, docker_name, sif_factory):
     """Test behavior when the SIF file is missing and `BABYSEG_SIF` unset."""
-    tool, mock = mock_factory(tool)
     tag = 'absent'
     monkeypatch.setenv('BABYSEG_TAG', tag)
 
     # Expect `pull` and `run` calls for SIF tools.
     assert docker.wrapper.main(argv=[]) == 0
-    assert mock.call_count == 2
+    assert tool.call_count == 2
 
     # Default image path and URL.
     d = pathlib.Path(docker.wrapper.__file__).parent
@@ -185,99 +181,92 @@ def test_sif_pull(mock_factory, tool, monkeypatch, docker_name, sif_factory):
     hub = f'docker://{docker_name}:{tag}'
 
     # Expect valid `pull` arguments.
-    (first,) = mock.call_args_list[0].args
-    assert first == (tool, 'pull', sif, hub)
+    (first,) = tool.call_args_list[0].args
+    assert first == (tool.path, 'pull', sif, hub)
 
 
-@pytest.mark.parametrize('tool', TOOLS_SIF)
-def test_sif_run(mock_factory, tool, monkeypatch, sif_factory):
+@pytest.mark.parametrize('tool', TOOLS_SIF, indirect=True)
+def test_sif_run(tool, monkeypatch, sif_factory):
     """Test behavior when the SIF file exists."""
-    tool, mock = mock_factory(tool)
-    sif = sif_factory(tool.parent, docker.wrapper.TAG, touch=True)
+    sif = sif_factory(tool.path.parent, docker.wrapper.TAG, touch=True)
     monkeypatch.setenv('BABYSEG_SIF', str(sif.parent))
 
-    # Expect `run` call  only.
+    # Expect `run` call only.
     assert docker.wrapper.main(argv=[]) == 0
-    mock.assert_called_once()
+    tool.assert_called_once()
 
     # Expect valid `run` arguments.
-    (call,) = mock.call_args.args
-    assert call[:2] == (tool, 'run')
+    (call,) = tool.call_args.args
+    assert call[:2] == (tool.path, 'run')
     assert '--pwd' in call
     assert '/mnt' in call
     assert f'{os.getcwd()}:/mnt' in call
     assert call[-1] == sif
 
 
-@pytest.mark.parametrize('tool', TOOLS_ALL)
-def test_bind_mount(mock_factory, tool, monkeypatch):
+@pytest.mark.parametrize('tool', TOOLS_ALL, indirect=True)
+def test_bind_mount(tool, monkeypatch):
     """Test explicit bind mount of `/mnt` inside container."""
-    tool, mock = mock_factory(tool)
     d = '/a/b/c'
     monkeypatch.setenv('BABYSEG_MNT', d)
 
     # Expect one or two calls.
     assert docker.wrapper.main(argv=[]) == 0
-    mock.assert_called()
+    tool.assert_called()
 
     # Expect bind mount in final `run` call.
-    (call,) = mock.call_args.args
+    (call,) = tool.call_args.args
     assert f'{d}:/mnt' in call
 
 
-@pytest.mark.parametrize('tool', TOOLS_SIF)
+@pytest.mark.parametrize('tool', TOOLS_SIF, indirect=True)
 @pytest.mark.parametrize('tag', ['1.2.3-cu130', '9.9'])
-def test_sif_gpu(mock_factory, tool, monkeypatch, tag):
+def test_sif_gpu(tool, monkeypatch, tag):
     """Test enabling GPU support via image tag."""
-    tool, mock = mock_factory(tool)
     monkeypatch.setenv('BABYSEG_TAG', tag)
     assert docker.wrapper.main(argv=[]) == 0
 
     # Expect GPU flag when `-cu` in tag.
-    mock.assert_called()
-    (call,) = mock.call_args.args
+    tool.assert_called()
+    (call,) = tool.call_args.args
     assert ('-cu' in tag) == ('--nv' in call)
 
 
-@pytest.mark.parametrize('tool', TOOLS_SIF)
-def test_error_code_on_pull(mock_factory, tool, monkeypatch):
+@pytest.mark.parametrize('tool', TOOLS_SIF, indirect=True)
+def test_error_code_on_pull(tool, monkeypatch):
     """Test if failure on SIF image `pull` returns the correct code."""
     # Set BABYSEG_SIF to a directory without SIF images.
-    tool, mock = mock_factory(tool)
-    monkeypatch.setenv('BABYSEG_SIF', str(tool.parent))
     code = 7
-    mock.return_value = subprocess.CompletedProcess(args='', returncode=code)
+    tool.return_value = subprocess.CompletedProcess(args='', returncode=code)
+    monkeypatch.setenv('BABYSEG_SIF', str(tool.path.parent))
 
     # Expect exit after failed pull.
     assert docker.wrapper.main(argv=[]) == code
-    mock.assert_called_once()
+    tool.assert_called_once()
 
 
-@pytest.mark.parametrize('tool', TOOLS_ALL)
-def test_error_code_on_run(mock_factory, tool, monkeypatch, sif_factory):
+@pytest.mark.parametrize('tool', TOOLS_ALL, indirect=True)
+def test_error_code_on_run(tool, monkeypatch, sif_factory):
     """Test if failure on `run` returns the correct code."""
-    tool, mock = mock_factory(tool)
     code = 13
-    mock.return_value = subprocess.CompletedProcess(args='', returncode=code)
-    sif = sif_factory(tool.parent, docker.wrapper.TAG, touch=True)
+    tool.return_value = subprocess.CompletedProcess(args='', returncode=code)
+    sif = sif_factory(tool.path.parent, docker.wrapper.TAG, touch=True)
     monkeypatch.setenv('BABYSEG_SIF', str(sif.parent))
 
     # Expect single call as SIF file exists.
     assert docker.wrapper.main(argv=[]) == code
-    mock.assert_called_once()
+    tool.assert_called_once()
 
 
-@pytest.mark.parametrize('tool', TOOLS_ALL)
-def test_arguments(mock_factory, tool, sif_factory, monkeypatch):
+@pytest.mark.parametrize('tool', TOOLS_ALL, indirect=True)
+def test_arguments(tool, sif_factory, monkeypatch):
     """Test if the wrapper forwards input arguments."""
     # Create SIF file, so SIF tools skip the `pull` call.
-    argv = ('-a', '-B','file.json')
-    tool, mock = mock_factory(tool)
-    sif = sif_factory(tool.parent, docker.wrapper.TAG, touch=True)
+    argv = ('-a', '-B', 'file.json')
+    sif = sif_factory(tool.path.parent, docker.wrapper.TAG, touch=True)
     monkeypatch.setenv('BABYSEG_SIF', str(sif.parent))
 
     assert docker.wrapper.main(argv) == 0
-    mock.assert_called_once()
-    (call,) = mock.call_args.args
-    print(call)
+    tool.assert_called_once()
+    (call,) = tool.call_args.args
     assert call[-len(argv):] == argv
